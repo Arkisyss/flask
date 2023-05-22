bec46aca8f3f67411cae3ad956394e04ce227511
Cowez <benjamincowez.pro@gmail.com>
2023-05-22 17:13:07 +0200

    commentaire

--- app.py
+++ app.py
@@ -10,11 +10,13 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
     
-# page de validation
+# Créé la route de la page de validation
 @app.route('/retour', methods=['POST']) 
 
-# Définition de la fonction de validation des données du formulaire
+# Création de la fonction principale 
+# Définition de la fonction de validation des données du formulaire, vérifaction des données, protection des données ainsi que renvoie vers la page de feedback.
 def form_valide():
+    
     # Fonction auxiliaire pour valider l'adresse email
     def email_valide(email):
         pattern = r'^[\w\.-]+@[\w\.-]+\.\w+$'
@@ -34,7 +36,7 @@ def form_valide():
     
     # Vérification des caractères dans "nom" et "prenom"
     if not re.match(r'^[A-Za-zÀ-ÿ\s-]+$', nom) or not re.match(r'^[A-Za-zÀ-ÿ\s-]+$', prenom):
-        return "Le nom et le prénom doivent contenir uniquement des lettres ou être rempli."
+        return "Le nom et le prénom doivent contenir uniquement des lettres et/ou être rempli."
 
     # Vérification de l'adresse email
     if not email_valide(email):
@@ -44,6 +46,7 @@ def form_valide():
     if not nom or not prenom or not email or not pays or not genre or not message:
         return "Veuillez remplir tous les champs du formulaire."
     
+    # Faire en sorte que si aucun sujet n'est sélectionné alors le choix par défaut est "Autre"
     if not sujets:
         sujets = ["Autre"]
     
@@ -51,8 +54,9 @@ def form_valide():
     if honeypot:
         return "Êtes-vous un robot ?"
     
+    # renvoyé toutes les infos entré sur la page retour.html pour avoir un feedback.
     return render_template('retour.html', prenom = prenom, nom = nom, email = email, pays = pays, genre = genre, message = message, sujets = sujets )
 
 # Lancement de l'app via le terminal
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
